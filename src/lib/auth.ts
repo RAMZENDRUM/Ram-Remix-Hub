c85397c281223cf701987673f7f9c48760cdc420
Your Name <your.email@example.com>
2025-12-04 23:22:32 +0530

    Fix session cookie settings for Vercel

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -52,6 +52,17 @@ export const authOptions: NextAuthOptions = {
     session: {
         strategy: "jwt",
     },
+    cookies: {
+        sessionToken: {
+            name: `next-auth.session-token`,
+            options: {
+                httpOnly: true,
+                sameSite: 'lax',
+                path: '/',
+                secure: process.env.NODE_ENV === 'production',
+            },
+        },
+    },
     pages: {
         signIn: "/auth",
     },
